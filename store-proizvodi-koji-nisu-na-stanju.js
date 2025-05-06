2286b853b39e560fffaed82d9b7bdac700992fdb
Branko Kovacevic <branko.kovacevic09@gmail.com>
2025-05-06 17:19:35 +0200

    Napravljen novi red za svaki artikal u tabeli; popunjene informacije za svaki artikal; crvenom bojom obojene oni artikli koji nisu na stanju

--- store-proizvodi-koji-nisu-na-stanju.js
+++ store-proizvodi-koji-nisu-na-stanju.js
@@ -11,8 +11,44 @@ class Artikal {
 
 
 
-let grafickaKartaGigatron = new Artikal(1, "GTX-4060", 780, true);
-let tvGigatron = new Artikal(2, "LG-TV", 880, false);
-let misGigatron = new Artikal(3, "Logitech-ZG-230", 30, true);
+const artikli = [
+    new Artikal(1, "Laptop", 800, true),
+    new Artikal(2, "Telefon", 300, false),
+    new Artikal(3, "Tablet", 400, true),
+    new Artikal(4, "Kamera", 600, false),
+];
 
-const artikliProdavnica = [grafickaKartaGigatron, tvGigatron, misGigatron];
+function prikaziArtikle() {
+    const tabela = document.getElementById("artikli");
+
+    for (let i = 0; i < artikli.length; i++) {
+        const artikal = artikli[i];
+
+        const red = document.createElement("tr");
+
+        const idCelija = document.createElement("td");
+        idCelija.textContent = artikal.id;
+
+        const nazivCelija = document.createElement("td");
+        nazivCelija.textContent = artikal.naziv;
+
+        const cenaCelija = document.createElement("td");
+        cenaCelija.textContent = artikal.cena + "RSD";
+
+        const dostupnostCelija = document.createElement("td")
+        dostupnostCelija.textContent = artikal.dostupnost ? "Na stanju" : "Nema na stanju";
+
+        if (!artikal.dostupnost) {
+            red.style.backgroundColor = "red";
+        }
+
+        red.appendChild(idCelija);
+        red.appendChild(nazivCelija);
+        red.appendChild(cenaCelija);
+        red.appendChild(dostupnostCelija);
+
+        tabela.appendChild(red);
+    }
+}
+
+window.onload = prikaziArtikle;
